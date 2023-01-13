53af74e1de655eed8d30d6ca23eb6f1c5f63374e
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-01-13 09:54:18 -0300

    feat: astro/image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import Icons from "unplugin-icons/vite";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [
@@ -10,4 +13,9 @@ export default defineConfig({
       }),
     ],
   },
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
